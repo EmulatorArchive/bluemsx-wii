c9f21fb65ffa1cb0ffd90bf25d4287ddc355e837
timbrug <timbrug@c2eab908-c631-11dd-927d-974589228060>
2008-12-09 21:55:52 +0000

    blueMSX Lite Wii. Based on SDL version of blueMSX Lite.

---
+++ trunk/source/Wii/WiiGlob.c
@@ -0,0 +1,388 @@
+/*****************************************************************************
+** $Source: WiiGlob.c,v $
+**
+** $Revision: 1.2 $
+**
+** $Date: 2006/06/24 02:27:08 $
+**
+** More info: http://www.bluemsx.com
+**
+** Copyright (C) 2003-2006 Daniel Vik
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+******************************************************************************
+*/
+#if 1
+
+#include "ArchGlob.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fat.h>
+#include <string.h>
+#include <sys/dir.h>
+#include <unistd.h>
+
+#define MAX_PATH FILENAME_MAX
+#define FILE_ATTRIBUTE_DIRECTORY 1
+#define INVALID_HANDLE_VALUE NULL
+
+typedef unsigned int DWORD;
+
+typedef void* HANDLE;
+
+typedef struct {
+    // Private
+    DIR_ITER *dirPointer;
+    struct stat fileStat;
+	char cPattern[MAX_PATH + 1];
+	// Public
+	char cFileName[MAX_PATH + 1];
+} WIN32_FIND_DATA;
+
+void GetCurrentDirectory(int max, char *path)
+{
+    (void)getcwd(path, max);
+}
+
+void SetCurrentDirectory(char *path)
+{
+    chdir(path);
+}
+
+void FindClose(HANDLE handle)
+{
+    WIN32_FIND_DATA *wfd = (WIN32_FIND_DATA *)handle;
+
+	if( !wfd || !wfd->dirPointer ) {
+		fprintf(stderr, "Couldn't close directory!\n");
+        return;
+	}
+	// Pass the DIR_ITER pointer to libfat and shut 'er down.
+	(void)dirclose(wfd->dirPointer);
+	wfd->dirPointer = NULL;
+}
+
+int FindNextFile(HANDLE handle, WIN32_FIND_DATA *wfd)
+{
+    (void)handle;
+
+	if( wfd == NULL || wfd->dirPointer == NULL ) {
+		fprintf(stderr, "Invalid find data in FinNextFile!\n");
+        return 0;
+	}
+
+	for(;;) {
+		if(dirnext(wfd->dirPointer, wfd->cFileName, &wfd->fileStat) != 0)
+		{
+            //fprintf(stderr, "No\n");
+			return 0; // No more files found
+		}else{
+			// Pattern validation
+			//fprintf(stderr, "Matching '%s' with '%s' ... ", wfd->cPattern, wfd->cFileName);
+			char *pp = wfd->cPattern;
+			char *pf = wfd->cFileName;
+			while( *pp != '\0' ) {
+				if( *pp == '?' && *pf != '\0' ) pf++;
+				if( *pp == '*' ) {
+					while( *pf != '\0' && *pf != *(pp+1) ) pf++;
+				}
+				if( *pp != '?' && *pp != '*' ) {
+				  if( toupper(*pp) != toupper(*pf) ) {
+					break; // No match
+				  }
+				  pf++;
+				}
+				pp++;
+				if( *pf == '\0' && *pp == '\0' ) {
+                    //fprintf(stderr, "Yes\n");
+					return 1; // Match
+				}
+			}
+		}
+	}
+    //fprintf(stderr, "Error\n");
+	return 0; // never get here
+}
+
+HANDLE FindFirstFile(const char *pattern, WIN32_FIND_DATA *wfd)
+{
+    char current_dir[MAX_PATH+1] = "/";
+
+    (void)getcwd(current_dir, sizeof(current_dir));
+    wfd->dirPointer = diropen(current_dir);
+	if( wfd->dirPointer == NULL ) {
+		fprintf(stderr, "Could not open directory '%s'!\n", current_dir);
+		return NULL; // diropen sets errno for us, so just return NULL.
+	}
+
+    strncpy(wfd->cPattern, pattern, MAX_PATH);
+
+    if( FindNextFile(NULL, wfd) ) { // Find first entry
+		return wfd;
+	}else{
+		FindClose(wfd);
+		return NULL;
+	}
+}
+
+DWORD GetFileAttributesWfd(WIN32_FIND_DATA *wfd)
+{
+    DWORD attr = 0;
+
+	if( !wfd || !wfd->dirPointer ) {
+		fprintf(stderr, "Invalid handle in GetFileAttributes!\n");
+        return 0;
+	}
+    //fprintf(stderr, "<%X>", wfd->fileStat.st_mode);
+    if( wfd->fileStat.st_mode & S_IFDIR ) {
+        attr |= FILE_ATTRIBUTE_DIRECTORY;
+    }
+
+	return attr;
+}
+
+// This glob only support very basic globbing, dirs in the patterns are only
+// supported without any wildcards
+
+ArchGlob* archGlob(const char* pattern, int flags)
+{
+    char oldPath[MAX_PATH];
+    const char* filePattern;
+    ArchGlob* glob;
+    WIN32_FIND_DATA wfd;
+    HANDLE handle;
+
+    GetCurrentDirectory(MAX_PATH, oldPath);
+
+    filePattern = strrchr(pattern, '/');
+    if (filePattern == NULL) {
+        filePattern = pattern;
+    }
+    else {
+        char relPath[MAX_PATH];
+        strcpy(relPath, pattern);
+        relPath[filePattern - pattern] = '\0';
+        pattern = filePattern + 1;
+        SetCurrentDirectory(relPath);
+    }
+
+    handle = FindFirstFile(pattern, &wfd);
+    if (handle == INVALID_HANDLE_VALUE) {
+        SetCurrentDirectory(oldPath);
+        return NULL;
+    }
+
+    glob = (ArchGlob*)calloc(1, sizeof(ArchGlob));
+
+    do {
+        DWORD fa;
+        if (0 == strcmp(wfd.cFileName, ".") || 0 == strcmp(wfd.cFileName, "..")) {
+            continue;
+        }
+        //fprintf(stderr, "found '%s' ... ", wfd.cFileName);
+		fa = GetFileAttributesWfd(&wfd);
+        if (((flags & ARCH_GLOB_DIRS) && (fa & FILE_ATTRIBUTE_DIRECTORY) != 0) ||
+            ((flags & ARCH_GLOB_FILES) && (fa & FILE_ATTRIBUTE_DIRECTORY) == 0))
+        {
+            char* path = (char*)malloc(MAX_PATH);
+            GetCurrentDirectory(MAX_PATH, path);
+            //strcat(path, "/");
+            strcat(path, wfd.cFileName);
+            //fprintf(stderr, "ok\n");
+
+            glob->count++;
+            glob->pathVector = realloc(glob->pathVector, sizeof(char*) * glob->count);
+            glob->pathVector[glob->count - 1] = path;
+        }else{
+            //fprintf(stderr, "bad attr (%d, %d)\n", flags, fa);
+        }
+        //archThreadSleep(1000);
+    } while (FindNextFile(handle, &wfd));
+
+    FindClose(handle);
+
+    SetCurrentDirectory(oldPath);
+
+    return glob;
+}
+
+void archGlobFree(ArchGlob* globHandle)
+{
+    int i;
+
+    if (globHandle == NULL) {
+        return;
+    }
+
+    for (i = 0; i < globHandle->count; i++) {
+        free(globHandle->pathVector[i]);
+    }
+    free(globHandle->pathVector);
+    free(globHandle);
+}
+
+#else
+#ifdef WINDOWS_HOST
+
+#include "ArchGlob.h"
+#include <windows.h>
+#include <stdlib.h>
+
+// This glob only support very basic globbing, dirs in the patterns are only
+// supported without any wildcards
+
+ArchGlob* archGlob(const char* pattern, int flags)
+{
+    char oldPath[MAX_PATH];
+    const char* filePattern;
+    ArchGlob* glob;
+    WIN32_FIND_DATA wfd;
+    HANDLE handle;
+
+    GetCurrentDirectory(MAX_PATH, oldPath);
+
+    filePattern = strrchr(pattern, '/');
+    if (filePattern == NULL) {
+        filePattern = pattern;
+    }
+    else {
+        char relPath[MAX_PATH];
+        strcpy(relPath, pattern);
+        relPath[filePattern - pattern] = '\0';
+        pattern = filePattern + 1;
+        SetCurrentDirectory(relPath);
+    }
+
+    handle = FindFirstFile(pattern, &wfd);
+    if (handle == INVALID_HANDLE_VALUE) {
+        SetCurrentDirectory(oldPath);
+        return NULL;
+    }
+
+    glob = (ArchGlob*)calloc(1, sizeof(ArchGlob));
+
+    do {
+        DWORD fa;
+        if (0 == strcmp(wfd.cFileName, ".") || 0 == strcmp(wfd.cFileName, "..")) {
+            continue;
+        }
+		fa = GetFileAttributes(wfd.cFileName);
+        if (((flags & ARCH_GLOB_DIRS) && (fa & FILE_ATTRIBUTE_DIRECTORY) != 0) ||
+            ((flags & ARCH_GLOB_FILES) && (fa & FILE_ATTRIBUTE_DIRECTORY) == 0))
+        {
+            char* path = (char*)malloc(MAX_PATH);
+            GetCurrentDirectory(MAX_PATH, path);
+            strcat(path, "\\");
+            strcat(path, wfd.cFileName);
+
+            glob->count++;
+            glob->pathVector = realloc(glob->pathVector, sizeof(char*) * glob->count);
+            glob->pathVector[glob->count - 1] = path;
+        }
+    } while (FindNextFile(handle, &wfd));
+
+    FindClose(handle);
+
+    SetCurrentDirectory(oldPath);
+
+    return glob;
+}
+
+void archGlobFree(ArchGlob* globHandle)
+{
+    int i;
+
+    if (globHandle == NULL) {
+        return;
+    }
+
+    for (i = 0; i < globHandle->count; i++) {
+        free(globHandle->pathVector[i]);
+    }
+    free(globHandle->pathVector);
+    free(globHandle);
+}
+
+#else
+
+#include "ArchGlob.h"
+#include "glob.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+ArchGlob* archGlob(const char* pattern, int flags)
+{
+    ArchGlob* globHandle;
+    glob_t g;
+    int rv;
+    int i;
+
+    rv = glob(pattern, GLOB_MARK, NULL, &g);
+    if (rv != 0) {
+        return NULL;
+    }
+
+    globHandle = (ArchGlob*)calloc(1, sizeof(ArchGlob));
+
+    for (i = 0; i < g.gl_pathc; i++) {
+        char* path = g.gl_pathv[i];
+        int len = strlen(path);
+
+        if ((flags & ARCH_GLOB_DIRS) && path[len - 1] == '/') {
+            char* storePath = calloc(1, len);
+            memcpy(storePath, path, len - 1);
+            globHandle->count++;
+            globHandle->pathVector = realloc(globHandle->pathVector, sizeof(char*) * globHandle->count);
+            globHandle->pathVector[globHandle->count - 1] = storePath;
+        }
+
+        if ((flags & ARCH_GLOB_FILES) && path[len - 1] != '/') {
+            char* storePath = calloc(1, len + 1);
+            memcpy(storePath, path, len);
+            globHandle->count++;
+            globHandle->pathVector = realloc(globHandle->pathVector, sizeof(char*) * globHandle->count);
+            globHandle->pathVector[globHandle->count - 1] = storePath;
+        }
+    }
+
+    globfree(&g);
+
+    return globHandle;
+}
+
+void archGlobFree(ArchGlob* globHandle)
+{
+    int i;
+
+    if (globHandle == NULL) {
+        return;
+    }
+
+    for (i = 0; i < globHandle->count; i++) {
+        free(globHandle->pathVector[i]);
+    }
+    if (globHandle->pathVector != NULL) {
+        free(globHandle->pathVector);
+    }
+    free(globHandle);
+}
+
+#endif
+#endif
---
+++ trunk/source/Wii/kbdlib.c
@@ -0,0 +1,474 @@
+/*****************************************************************************
+** $Source: kbdlib.c,v $
+**
+** $Revision: 0.0 $
+**
+** $Date: 2008/12/09 00:00:00 $
+**
+** Copyright (C) 2008 Tim Brugman
+**
+**  This software is provided 'as-is', without any express or implied
+**  warranty.  In no event will the authors be held liable for any damages
+**  arising from the use of this software.
+**
+**  Permission is granted to anyone to use this software for any purpose,
+**  including commercial applications, and to alter it and redistribute it
+**  freely, subject to the following restrictions:
+**
+**  1. The origin of this software must not be misrepresented; you must not
+**     claim that you wrote the original software. If you use this software
+**     in a product, an acknowledgment in the product documentation would be
+**     appreciated but is not required.
+**  2. Altered source versions must be plainly marked as such, and must not be
+**     misrepresented as being the original software.
+**  3. This notice may not be removed or altered from any source distribution.
+**
+******************************************************************************
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <wiiuse/wpad.h>
+
+#include "kbdlib.h"
+
+typedef struct {
+	KEY key;
+	int code;
+} KEYCODE;
+
+typedef struct {
+	KEY key_a;
+	KEY key_b;
+	int code;
+} PADCODE;
+
+typedef struct {
+	u32 message;
+	u32 id; // in fact it is a direction
+	u8 modifiers;
+	u8 unknown;
+	u8 keys[6];
+} key_data_t;
+
+struct _kbd_data {
+    key_data_t msg;
+    u8 modifiers;
+	u8 keys[6];
+    u8 keystatus[2][KEY_LAST];
+    int keyidx;
+    int fhandle;
+    int connected;
+    int quiting;
+    u32 wpad[2];
+};
+
+static const char *keynames[KEY_LAST];
+
+static PADCODE wpad[] =
+{
+	{KEY_JOY1_BUTTON1, KEY_JOY2_BUTTON1, (WPAD_BUTTON_A     | WPAD_CLASSIC_BUTTON_A |
+                                          WPAD_BUTTON_1     | WPAD_CLASSIC_BUTTON_X)    },
+	{KEY_JOY1_BUTTON2, KEY_JOY2_BUTTON2, (WPAD_BUTTON_B     | WPAD_CLASSIC_BUTTON_B |
+                                          WPAD_BUTTON_2     | WPAD_CLASSIC_BUTTON_Y)    },
+	{KEY_JOY1_UP,      KEY_JOY2_UP,      (WPAD_BUTTON_RIGHT | WPAD_CLASSIC_BUTTON_UP)   },
+	{KEY_JOY1_DOWN,    KEY_JOY2_DOWN,    (WPAD_BUTTON_LEFT  | WPAD_CLASSIC_BUTTON_DOWN) },
+	{KEY_JOY1_LEFT,    KEY_JOY2_LEFT,    (WPAD_BUTTON_UP    | WPAD_CLASSIC_BUTTON_LEFT) },
+	{KEY_JOY1_RIGHT,   KEY_JOY2_RIGHT,   (WPAD_BUTTON_DOWN  | WPAD_CLASSIC_BUTTON_RIGHT)},
+	{KEY_JOY1_HOME,    KEY_JOY2_HOME,    (WPAD_BUTTON_HOME  | WPAD_CLASSIC_BUTTON_HOME) },
+	{KEY_JOY1_PLUS,    KEY_JOY2_PLUS,    (WPAD_BUTTON_PLUS  | WPAD_CLASSIC_BUTTON_PLUS) },
+    {KEY_JOY1_MINUS,   KEY_JOY2_MINUS,   (WPAD_BUTTON_MINUS | WPAD_CLASSIC_BUTTON_MINUS)},
+	{KEY_NONE, KEY_NONE, 0}
+};
+
+static KEYCODE mods[] =
+{
+	{KEY_LCTRL,   0x01},
+	{KEY_LSHIFT,  0x02},
+	{KEY_LALT,    0x04},
+	{KEY_LWIN,    0x08},
+	{KEY_RCTRL,   0x10},
+	{KEY_RSHIFT,  0x20},
+	{KEY_RALT,    0x40},
+	{KEY_RWIN,    0x80},
+	{KEY_NONE, 0}
+};
+
+static KEYCODE keys[] =
+{
+	{KEY_A, 4},
+	{KEY_B, 5},
+	{KEY_C, 6},
+	{KEY_D, 7},
+	{KEY_E, 8},
+	{KEY_F, 9},
+	{KEY_G, 10},
+	{KEY_H, 11},
+	{KEY_I, 12},
+	{KEY_J, 13},
+	{KEY_K, 14},
+	{KEY_L, 15},
+	{KEY_M, 16},
+	{KEY_N, 17},
+	{KEY_O, 18},
+	{KEY_P, 19},
+	{KEY_Q, 20},
+	{KEY_R, 21},
+	{KEY_S, 22},
+	{KEY_T, 23},
+	{KEY_U, 24},
+	{KEY_V, 25},
+	{KEY_W, 26},
+	{KEY_X, 27},
+	{KEY_Y, 28},
+	{KEY_Z, 29},
+	{KEY_1, 30},
+	{KEY_2, 31},
+	{KEY_3, 32},
+	{KEY_4, 33},
+	{KEY_5, 34},
+	{KEY_6, 35},
+	{KEY_7, 36},
+	{KEY_8, 37},
+	{KEY_9, 38},
+	{KEY_0, 39},
+	{KEY_RETURN, 40},
+	{KEY_ESCAPE, 41},
+	{KEY_BACKSPACE, 42},
+	{KEY_TAB, 43},
+	{KEY_SPACE, 44},
+	{KEY_MINUS, 45},
+	{KEY_EQUALS, 46},
+	{KEY_LEFTBRACKET, 47},
+	{KEY_RIGHTBRACKET, 48},
+	{KEY_BACKSLASH, 49},
+	{KEY_SEMICOLON, 51},
+	{KEY_QUOTE, 52},
+	{KEY_BACKQUOTE, 53},
+	{KEY_COMMA, 54},
+	{KEY_PERIOD, 55},
+	{KEY_SLASH, 56},
+	{KEY_CAPSLOCK, 57},
+	{KEY_F1, 58},
+	{KEY_F2, 59},
+	{KEY_F3, 60},
+	{KEY_F4, 61},
+	{KEY_F5, 62},
+	{KEY_F6, 63},
+	{KEY_F7, 64},
+	{KEY_F8, 65},
+	{KEY_F9, 66},
+	{KEY_F10, 67},
+	{KEY_F11, 68},
+	{KEY_F12, 69},
+	{KEY_PRINT, 70},
+	{KEY_SCROLLOCK, 71},
+	{KEY_PAUSE, 72},
+	{KEY_INSERT, 73},
+	{KEY_HOME, 74},
+	{KEY_PAGEUP, 75},
+	{KEY_DELETE, 76},
+	{KEY_END, 77},
+	{KEY_PAGEDOWN, 78},
+	{KEY_RIGHT, 79},
+	{KEY_LEFT, 80},
+	{KEY_DOWN, 81},
+	{KEY_UP, 82},
+	{KEY_NUMLOCK, 83},
+	{KEY_KP_DIVIDE, 84},
+	{KEY_KP_MULTIPLY, 85},
+	{KEY_KP_MINUS, 86},
+	{KEY_KP_PLUS, 87},
+	{KEY_KP_ENTER, 88},
+	{KEY_KP1, 89},
+	{KEY_KP2, 90},
+	{KEY_KP3, 91},
+	{KEY_KP4, 92},
+	{KEY_KP5, 93},
+	{KEY_KP6, 94},
+	{KEY_KP7, 95},
+	{KEY_KP8, 96},
+	{KEY_KP9, 97},
+	{KEY_KP0, 98},
+	{KEY_KP_PERIOD, 99},
+	{KEY_KP_EQUALS, 100},
+	{KEY_NONE, 0}
+};
+
+static void FillKeyNames(void)
+{
+    /* Fill in the blanks in keynames */
+    keynames[KEY_BACKSPACE] = "backspace";
+    keynames[KEY_TAB]       = "tab";
+    keynames[KEY_CLEAR]     = "clear";
+    keynames[KEY_RETURN]    = "return";
+    keynames[KEY_PAUSE]     = "pause";
+    keynames[KEY_ESCAPE]    = "escape";
+    keynames[KEY_SPACE]     = "space";
+    keynames[KEY_EXCLAIM]   = "!";
+    keynames[KEY_QUOTEDBL]  = "\"";
+    keynames[KEY_HASH]      = "#";
+    keynames[KEY_DOLLAR]    = "$";
+    keynames[KEY_AMPERSAND] = "&";
+    keynames[KEY_QUOTE]     = "'";
+    keynames[KEY_LEFTPAREN] = "(";
+    keynames[KEY_RIGHTPAREN]= ")";
+    keynames[KEY_ASTERISK]  = "*";
+    keynames[KEY_PLUS]      = "+";
+    keynames[KEY_COMMA]     = ",";
+    keynames[KEY_MINUS]     = "-";
+    keynames[KEY_PERIOD]    = ".";
+    keynames[KEY_SLASH]     = "/";
+    keynames[KEY_0]         = "0";
+    keynames[KEY_1]         = "1";
+    keynames[KEY_2]         = "2";
+    keynames[KEY_3]         = "3";
+    keynames[KEY_4]         = "4";
+    keynames[KEY_5]         = "5";
+    keynames[KEY_6]         = "6";
+    keynames[KEY_7]         = "7";
+    keynames[KEY_8]         = "8";
+    keynames[KEY_9]         = "9";
+    keynames[KEY_COLON]     = ":";
+    keynames[KEY_SEMICOLON] = ";";
+    keynames[KEY_LESS]      = "<";
+    keynames[KEY_EQUALS]    = "=";
+    keynames[KEY_GREATER]   = ">";
+    keynames[KEY_QUESTION]  = "?";
+    keynames[KEY_AT]        = "@";
+    keynames[KEY_LEFTBRACKET] = "[";
+    keynames[KEY_BACKSLASH] = "\\";
+    keynames[KEY_RIGHTBRACKET] = "]";
+    keynames[KEY_CARET]     = "^";
+    keynames[KEY_UNDERSCORE] = "_";
+    keynames[KEY_BACKQUOTE] = "`";
+    keynames[KEY_A]         = "a";
+    keynames[KEY_B]         = "b";
+    keynames[KEY_C]         = "c";
+    keynames[KEY_D]         = "d";
+    keynames[KEY_E]         = "e";
+    keynames[KEY_F]         = "f";
+    keynames[KEY_G]         = "g";
+    keynames[KEY_H]         = "h";
+    keynames[KEY_I]         = "i";
+    keynames[KEY_J]         = "j";
+    keynames[KEY_K]         = "k";
+    keynames[KEY_L]         = "l";
+    keynames[KEY_M]         = "m";
+    keynames[KEY_N]         = "n";
+    keynames[KEY_O]         = "o";
+    keynames[KEY_P]         = "p";
+    keynames[KEY_Q]         = "q";
+    keynames[KEY_R]         = "r";
+    keynames[KEY_S]         = "s";
+    keynames[KEY_T]         = "t";
+    keynames[KEY_U]         = "u";
+    keynames[KEY_V]         = "v";
+    keynames[KEY_W]         = "w";
+    keynames[KEY_X]         = "x";
+    keynames[KEY_Y]         = "y";
+    keynames[KEY_Z]         = "z";
+    keynames[KEY_DELETE]    = "delete";
+
+    keynames[KEY_KP0]       = "[0]";
+    keynames[KEY_KP1]       = "[1]";
+    keynames[KEY_KP2]       = "[2]";
+    keynames[KEY_KP3]       = "[3]";
+    keynames[KEY_KP4]       = "[4]";
+    keynames[KEY_KP5]       = "[5]";
+    keynames[KEY_KP6]       = "[6]";
+    keynames[KEY_KP7]       = "[7]";
+    keynames[KEY_KP8]       = "[8]";
+    keynames[KEY_KP9]       = "[9]";
+    keynames[KEY_KP_PERIOD] = "[.]";
+    keynames[KEY_KP_DIVIDE] = "[/]";
+    keynames[KEY_KP_MULTIPLY] = "[*]";
+    keynames[KEY_KP_MINUS]  = "[-]";
+    keynames[KEY_KP_PLUS]   = "[+]";
+    keynames[KEY_KP_ENTER]  = "enter";
+    keynames[KEY_KP_EQUALS] = "equals";
+
+    keynames[KEY_UP]        = "up";
+    keynames[KEY_DOWN]      = "down";
+    keynames[KEY_RIGHT]     = "right";
+    keynames[KEY_LEFT]      = "left";
+    keynames[KEY_DOWN]      = "down";
+    keynames[KEY_INSERT]    = "insert";
+    keynames[KEY_HOME]      = "home";
+    keynames[KEY_END]       = "end";
+    keynames[KEY_PAGEUP]    = "page up";
+    keynames[KEY_PAGEDOWN]  = "page down";
+
+    keynames[KEY_F1]        = "f1";
+    keynames[KEY_F2]        = "f2";
+    keynames[KEY_F3]        = "f3";
+    keynames[KEY_F4]        = "f4";
+    keynames[KEY_F5]        = "f5";
+    keynames[KEY_F6]        = "f6";
+    keynames[KEY_F7]        = "f7";
+    keynames[KEY_F8]        = "f8";
+    keynames[KEY_F9]        = "f9";
+    keynames[KEY_F10]       = "f10";
+    keynames[KEY_F11]       = "f11";
+    keynames[KEY_F12]       = "f12";
+
+    keynames[KEY_NUMLOCK]   = "numlock";
+    keynames[KEY_CAPSLOCK]  = "caps lock";
+    keynames[KEY_SCROLLOCK] = "scroll lock";
+    keynames[KEY_RSHIFT]    = "right shift";
+    keynames[KEY_LSHIFT]    = "left shift";
+    keynames[KEY_RCTRL]     = "right ctrl";
+    keynames[KEY_LCTRL]     = "left ctrl";
+    keynames[KEY_RALT]      = "right alt";
+    keynames[KEY_LALT]      = "left alt";
+    keynames[KEY_LWIN]      = "left win";
+    keynames[KEY_RWIN]      = "right win";
+    keynames[KEY_MODE]      = "alt gr";
+    keynames[KEY_COMPOSE]   = "compose";
+
+    keynames[KEY_PRINT]     = "print screen";
+
+    keynames[KEY_JOY1_BUTTON1] = "joy1a";
+    keynames[KEY_JOY1_BUTTON2] = "joy1b";
+    keynames[KEY_JOY1_UP]      = "joy1up";
+    keynames[KEY_JOY1_DOWN]    = "joy1down";
+    keynames[KEY_JOY1_LEFT]    = "joy1left";
+    keynames[KEY_JOY1_RIGHT]   = "joy1right";
+    keynames[KEY_JOY1_HOME]    = "joy1home";
+    keynames[KEY_JOY1_PLUS]    = "joy1plus";
+    keynames[KEY_JOY1_MINUS]   = "joy1min";
+
+    keynames[KEY_JOY2_BUTTON1] = "joy2a";
+    keynames[KEY_JOY2_BUTTON2] = "joy2b";
+    keynames[KEY_JOY2_UP]      = "joy2up";
+    keynames[KEY_JOY2_DOWN]    = "joy2down";
+    keynames[KEY_JOY2_LEFT]    = "joy2left";
+    keynames[KEY_JOY2_RIGHT]   = "joy2right";
+    keynames[KEY_JOY2_HOME]    = "joy2home";
+    keynames[KEY_JOY2_PLUS]    = "joy2plus";
+    keynames[KEY_JOY2_MINUS]   = "joy2min";
+}
+
+s32 keyboardCallback(int ret, void *arg)
+{
+    KBDHANDLE hndl = (KBDHANDLE)arg;
+    if( hndl->quiting ) {
+        return 0;
+    }
+    switch( hndl->msg.message ) {
+        case 2: /* keys */
+            hndl->modifiers = hndl->msg.modifiers;
+            memcpy(hndl->keys, hndl->msg.keys, 6);
+            break;
+        case 1: /* disconnect */
+            hndl->connected = 0;
+            hndl->modifiers = 0;
+            memset(hndl->keys, 0, 6);
+            break;
+        case 0: /* connect */
+            hndl->connected = 1;
+            hndl->modifiers = 0;
+            memset(hndl->keys, 0, 6);
+            break;
+    }
+    IOS_IoctlAsync(hndl->fhandle, 1, (void *)&hndl->msg, 16, (void *)&hndl->msg, 16, keyboardCallback, arg);
+    return 0;
+}
+
+int KBD_IsConnected(KBDHANDLE hndl)
+{
+    return hndl->connected;
+}
+
+const char *KBD_GetKeyName(KEY key)
+{
+    const char *keyname;
+
+    keyname = NULL;
+    if ( key < KEY_LAST ) {
+        keyname = keynames[key];
+    }
+    if ( keyname == NULL ) {
+        keyname = "unknown key";
+    }
+    return keyname;
+}
+
+void KBD_GetKeys(KBDHANDLE hndl, KBD_CALLBACK cb)
+{
+	int i, k;
+    int idx_prev = hndl->keyidx;
+    int idx_new  = hndl->keyidx ^ 1;
+
+    /* fill new key status array */
+    memset(hndl->keystatus[idx_new], 0, sizeof(hndl->keystatus[0]));
+    for(i = 0; i < 6; i++) {
+        if( hndl->keys[i] != 0 ) {
+        	for(k = 0; keys[k].key != KEY_NONE; k++)	{
+        		if( keys[k].code == hndl->keys[i] ) {
+        			hndl->keystatus[idx_new][keys[k].key] = 1;
+                    break;
+        		}
+        	}
+        }
+	}
+    for(i = 0; mods[i].key != KEY_NONE; i++)  {
+        if( (hndl->modifiers & mods[i].code) != 0  ) {
+            hndl->keystatus[idx_new][mods[i].key] = 1;
+        }
+    }
+    /* handle WPAD keys */
+    WPAD_ScanPads();
+    hndl->wpad[0] = WPAD_ButtonsHeld(0);
+    hndl->wpad[1] = WPAD_ButtonsHeld(1);
+    for(i = 0; wpad[i].key_a != KEY_NONE; i++)  {
+        if( (hndl->wpad[0] & wpad[i].code) != 0  ) {
+            hndl->keystatus[idx_new][wpad[i].key_a] = 1;
+        }
+        if( (hndl->wpad[1] & wpad[i].code) != 0  ) {
+            hndl->keystatus[idx_new][wpad[i].key_b] = 1;
+        }
+    }
+    /* compare with previous and call for each difference */
+    for(i = 0; i < KEY_LAST; i++) {
+        if( hndl->keystatus[idx_prev][i] != hndl->keystatus[idx_new][i] ) {
+            cb(hndl, (KEY)i, hndl->keystatus[idx_new][i]);
+        }
+    }
+    /* switch new<->previous */
+    hndl->keyidx ^= 1;
+}
+
+int KBD_GetKeyStatus(KBDHANDLE hndl, KEY key)
+{
+    return hndl->keystatus[hndl->keyidx][key];
+}
+
+void KBD_DeInit(KBDHANDLE hndl)
+{
+    hndl->quiting = 1;
+}
+
+KBDHANDLE KBD_Init(void)
+{
+    static int wpad_initialized = 0;
+    int dev;
+    FillKeyNames();
+    if( !wpad_initialized ) {
+    	WPAD_Init();
+        wpad_initialized = 1;
+    }
+    dev = IOS_Open("/dev/usb/kbd", 1);
+    if( dev >= 0 ) {
+        KBDHANDLE hndl = (KBDHANDLE)memalign(32, sizeof(KBDDATA));
+        memset(hndl, 0, sizeof(KBDDATA));
+        hndl->fhandle = dev;
+        IOS_IoctlAsync(hndl->fhandle, 1, (void *)&hndl->msg, 16, (void *)&hndl->msg, 16, keyboardCallback, (void*)hndl);
+        return hndl;
+    }else{
+        return NULL;
+    }
+}
+
