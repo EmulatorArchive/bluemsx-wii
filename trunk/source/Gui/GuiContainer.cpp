7e5ea075c2cde92c9143091257e9dcf4e316280f
timbrug <timbrug@c2eab908-c631-11dd-927d-974589228060>
2008-12-21 14:45:33 +0000

    Updated image references.

--- trunk/source/Gui/GuiContainer.cpp
+++ trunk/source/Gui/GuiContainer.cpp
@@ -1,6 +1,6 @@
 
 #include "GuiContainer.h"
-#include "container.h"
+#include "image_container.h"
 
 #define TILE_SIZE 12
 
@@ -11,7 +11,7 @@ GuiContainer::GuiContainer( int posx,  int posy, int width, int height)
 	cols = (width + TILE_SIZE/2) /TILE_SIZE;
 	rows = (height + TILE_SIZE/2) / TILE_SIZE;
 	win_layer = new TiledLayer(cols+1,rows+1,0);
-	win_img.LoadImage(container);
+	win_img.LoadImage(image_container);
 	win_layer->SetStaticTileset(&win_img, TILE_SIZE, TILE_SIZE);
     win_layer->SetTransparency(128);
 	SetPosition(posx, posy);
