6b9ec6dab2aaf27ba3f06ab956b7a7b7fb55c2b2
timbrug <timbrug@c2eab908-c631-11dd-927d-974589228060>
2009-08-05 22:51:32 +0000

    Release Build: V1.0RC

--- source/Wii/version.h
+++ source/Wii/version.h
@@ -3,8 +3,10 @@
 
 #include "revision.inc"
 
-#if REVISION_NUMBER==999
-#define VERSION_AS_STRING "V1.0.0"
+#if REVISION_NUMBER==109
+#define VERSION_AS_STRING "V1.0RC"
+#elif REVISION_NUMBER==999
+#define VERSION_AS_STRING "V1.0"
 #else
 #define VERSION_AS_STRING "Build "REVISION_NUMBER_STRING
 #endif
